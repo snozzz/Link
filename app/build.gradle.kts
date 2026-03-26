5860501388cfe7ec8e826d69f26cd70efec47c80
Snoozywqe <838009959@qq.com>
2026-03-27 01:10:45 +0800

    fix: rely on system resize for keyboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.snozzz.link"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.2.2"
+        versionCode = 6
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/snozzz/link/MainActivity.kt
+++ app/src/main/java/com/snozzz/link/MainActivity.kt
@@ -3,13 +3,11 @@ package com.snozzz.link
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
 import com.snozzz.link.ui.LinkApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             LinkApp()
         }
